45a820b464c641f70581597e4a51153f19eb8e61
Emericard <emericmartin32@gmail.com>
2023-02-03 18:05:21 +0100

    AA

--- bindings/python/samples/countdown.py
+++ bindings/python/samples/countdown.py
@@ -11,6 +11,7 @@ class Countdown(SampleBase):
     def __init__(self, *args, **kwargs):
         super(Countdown, self).__init__(*args, **kwargs)
 
+    
     def calculate_delta(self):
         """Calculates the number of days until next xmas"""
         now = datetime.datetime.now()
@@ -20,22 +21,19 @@ class Countdown(SampleBase):
         hours = delta.seconds // 3600
         minutes = (delta.seconds -3600*hours) // 60
         seconds = (delta.seconds -3600*hours - 60*minutes) // 60
-        return [days, hours, minutes, seconds]
-
-    def countdown(self):
-        offscreen_canvas = self.matrix.CreateFrameCanvas()
-        font = graphics.Font()
-        delta = self.calculate_delta()
-        font.LoadFont("../../../fonts/7x13.bdf")
-        string = str(delta[0]) + "d " + str(delta[1]) + "h " + str(delta[2]) + "min " + str(delta[3]) + "s "
-        len = graphics.DrawText(offscreen_canvas, font, 10, 10, graphics.Color(255,255,255), string)
-
-        offscreen_canvas = self.matrix.SwapOnVSync(offscreen_canvas)
+        return [days, hours, minutes, seconds]        
+        
     
     def run(self):
+        offscreen_canvas = self.matrix.CreateFrameCanvas()
+        font = graphics.Font()
+        font.LoadFont("../../../fonts/7x13.bdf")
         while True :
-            self.countdown()
+            delta = self.calculate_delta()
+            string = str(delta[0]) + "d " + str(delta[1]) + "h " + str(delta[2]) + "min " + str(delta[3]) + "s "
+            len = graphics.DrawText(offscreen_canvas, font, 10, 10, graphics.Color(255,255,255), string)
 
+            offscreen_canvas = self.matrix.SwapOnVSync(offscreen_canvas)
 
 # Main function
 if __name__ == "__main__":
