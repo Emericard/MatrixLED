4da7559b5c1b74c1d87d417607ff13a5d0fded71
Emericard <emericmartin32@gmail.com>
2023-02-03 17:40:16 +0100

    AA

--- bindings/python/samples/countdown.py
+++ bindings/python/samples/countdown.py
@@ -11,31 +11,19 @@ class Countdown(SampleBase):
     def __init__(self, *args, **kwargs):
         super(Countdown, self).__init__(*args, **kwargs)
 
-    def calculate_days_to_xmas(self):
+    def calculate_delta(self):
         """Calculates the number of days until next xmas"""
         now = datetime.datetime.now()
-        year = now.year
-        if now > datetime.datetime(year=year, month=12, day=25):
-            # Account for the days after xmas of the remaining year
-            year = year + 1
-        delta = now - datetime.datetime(year=year, month=12, day=25)
-        return abs(int(delta.days))
-    
-    def calculate_seconds(self):
-        now = datetime.datetime.now()
-        seconds_total = now.second
-        hours = seconds_total // 3600
-        minutes = (seconds_total - hours*3600) // 60
-        seconds = seconds_total - hours*3600 - minutes*60
-        return [hours, minutes, seconds]
+        deadline = datetime.datetime(year=2023, month=12, day=25)
+        delta = deadline - now
+        return [int(delta.days), int(delta.hour), int(delta.minute), int(delta.second)]
 
     def countdown(self):
-        chiffre = self.calculate_days_to_xmas()
         offscreen_canvas = self.matrix.CreateFrameCanvas()
         font = graphics.Font()
-        seconds_total = self.calculate_seconds()
+        delta = self.calculate_delta()
         font.LoadFont("../../../fonts/7x13.bdf")
-        string = str(chiffre) + "d " + str(seconds_total[0]) + "h " + str(seconds_total[1]) + "min " + str(seconds_total[2]) + "s "
+        string = str(delta[0]) + "d " + str(delta[1]) + "h " + str(delta[2]) + "min " + str(delta[3]) + "s "
         len = graphics.DrawText(offscreen_canvas, font, 10, 10, graphics.Color(255,255,255), string)
 
         offscreen_canvas = self.matrix.SwapOnVSync(offscreen_canvas)
