30d9e35212c04d6402ab4294163ae042e36e56f1
Emericard <emericmartin32@gmail.com>
2023-02-03 18:30:16 +0100

    AA

--- bindings/python/samples/countdown.py
+++ bindings/python/samples/countdown.py
@@ -32,9 +32,10 @@ class Countdown(SampleBase):
             
             delta = self.calculate_delta()
             string = str(delta[0]) + "d " + str(delta[1]) + "h " + str(delta[2]) + "min " + str(delta[3]) + "s "
-            len = graphics.DrawText(offscreen_canvas, font, 10, 10, graphics.Color(255,255,255), string)
+            
             time.sleep(2)
             offscreen_canvas.Clear()
+            len = graphics.DrawText(offscreen_canvas, font, 10, 10, graphics.Color(255,255,255), string)
             offscreen_canvas = self.matrix.SwapOnVSync(offscreen_canvas)
             time.sleep(2)
 
