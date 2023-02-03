a5648a1afc5849437adc23e166f156d52db54a6e
Coline <coline.droxler@epfl.ch>
2023-02-03 16:23:20 +0100

    frame

--- bindings/python/samples/animation.py
+++ bindings/python/samples/animation.py
@@ -6,10 +6,13 @@ from imageViewer import display_image
 from rgbmatrix import RGBMatrix, RGBMatrixOptions
 from PIL import Image
 
+frame = len(sys.arg) 
+
 if len(sys.argv) < 2:
     sys.exit("Require an image argument")
 else:
-    image_file = sys.argv[1]
+    for i in range(frame): 
+        image_file = sys.argv[i+1]
 
 display_image(image_file)
 
