e3d591e1c86ab85aed07d8478e91ccec7046064b
Coline <coline.droxler@epfl.ch>
2023-02-03 15:55:37 +0100

    test fonction image

--- bindings/python/samples/imageViewer.py
+++ bindings/python/samples/imageViewer.py
@@ -5,23 +5,25 @@ import sys
 from rgbmatrix import RGBMatrix, RGBMatrixOptions
 from PIL import Image
 
-if len(sys.argv) < 2:
-    sys.exit("Require an image argument")
-else:
-    image_file = sys.argv[1]
+def display_image(): 
 
-image = Image.open(image_file)
+    if len(sys.argv) < 2:
+        sys.exit("Require an image argument")
+    else:
+        image_file = sys.argv[1]
 
-# Configuration for the matrix
-options = RGBMatrixOptions()
-options.rows = 32
-options.cols = 64
-options.chain_length = 2
-options.gpio_slowdown = 5
-options.parallel = 1
-options.hardware_mapping = 'adafruit-hat'  # If you have an Adafruit HAT: 'adafruit-hat'
+    image = Image.open(image_file)
 
-matrix = RGBMatrix(options = options)
+    # Configuration for the matrix
+    options = RGBMatrixOptions()
+    options.rows = 32
+    options.cols = 64
+    options.chain_length = 2
+    options.gpio_slowdown = 5
+    options.parallel = 1
+    options.hardware_mapping = 'adafruit-hat'  # If you have an Adafruit HAT: 'adafruit-hat'
 
-matrix.SetImage(image.convert('RGB'))
+    matrix = RGBMatrix(options = options)
+
+    matrix.SetImage(image.convert('RGB'))
 
