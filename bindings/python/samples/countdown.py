ed4524099b2d5525166dc64b3f86be1f3b984a72
Emericard <emericmartin32@gmail.com>
2023-02-07 11:59:58 +0100

    AA

--- bindings/python/samples/countdown.py
+++ bindings/python/samples/countdown.py
@@ -73,7 +73,7 @@ graphics.Color(252, 229, 6),
 graphics.Color(255, 234, 0)]
 
         now = datetime.datetime.now()
-        pos = ceil((64*now.second/60))
+        pos = ceil((150*now.second/60)) // 64
         len = graphics.DrawText(canvas, font, pos, 10, colors[ceil(31*now.second/60)], my_text)
 
 
