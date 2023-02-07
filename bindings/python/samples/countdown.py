26ae3bd0d0605fd3ea4ee8d9df985cf7a6f50547
Emericard <emericmartin32@gmail.com>
2023-02-07 11:31:28 +0100

    AA

--- bindings/python/samples/countdown.py
+++ bindings/python/samples/countdown.py
@@ -4,13 +4,15 @@ from samplebase import SampleBase
 from rgbmatrix import graphics
 import sys
 import time
+from math import ceil
 import datetime
 
 
 class Countdown(SampleBase):
     def __init__(self, *args, **kwargs):
         super(Countdown, self).__init__(*args, **kwargs)
-    
+        self.parser.add_argument("-t", "--runtext", help="The text to scroll on the RGB LED panel", default="Hello world!")
+
     def calculate_delta(self):
         """Calculates the number of days until next xmas"""
         now = datetime.datetime.now()
@@ -22,21 +24,73 @@ class Countdown(SampleBase):
         seconds = delta.seconds -3600*hours - 60*minutes
         return [days, hours, minutes, seconds]        
     
-
-    
-    def run(self):
-        offscreen_canvas = self.matrix.CreateFrameCanvas()
+    def countdown(self,canvas, x = 20, y = 20) :
         font = graphics.Font()
-        offscreen_canvas.brightness = 20
+        canvas.brightness = 20
         font.LoadFont("../../../fonts/7x13.bdf")
         delta = self.calculate_delta()
         string = str(delta[0]) + "d " + str(delta[1]) + "h " + str(delta[2]) + "min " + str(delta[3]) + "s "
-        len = graphics.DrawText(offscreen_canvas, font, 20, 20, graphics.Color(255,255,255), string)
-        return len
+        len = graphics.DrawText(canvas, font, x, y, graphics.Color(255,255,255), string)
+        canvas = self.matrix.SwapOnVSync(canvas)
+    
+    def run(self):
+        offscreen_canvas = self.matrix.CreateFrameCanvas()
+        while True :
+            self.countdown(offscreen_canvas, 20,20)
+        return
+    
+    def runtext(self):
+        offscreen_canvas = self.matrix.CreateFrameCanvas()
+        font = graphics.Font()
+        font.LoadFont("../../../fonts/7x13.bdf")
+        my_text = self.args.text
+        colors = [graphics.Color(171, 71, 188),
+graphics.Color(174, 76, 182),
+graphics.Color(176, 82, 176),
+graphics.Color(179, 87, 170),
+graphics.Color(182, 92, 164),
+graphics.Color(185, 97, 158),
+graphics.Color(187, 103, 152),
+graphics.Color(190, 108, 146),
+graphics.Color(193, 113, 139),
+graphics.Color(195, 118, 133),
+graphics.Color(198, 124, 127),
+graphics.Color(201, 129, 121),
+graphics.Color(204, 134, 115),
+graphics.Color(206, 139, 109),
+graphics.Color(209, 145, 103),
+graphics.Color(212, 150, 97),
+graphics.Color(214, 155, 91),
+graphics.Color(217, 160, 85),
+graphics.Color(220, 166, 79),
+graphics.Color(222, 171, 73),
+graphics.Color(225, 176, 67),
+graphics.Color(228, 181, 61),
+graphics.Color(231, 187, 55),
+graphics.Color(233, 192, 49),
+graphics.Color(236, 197, 42),
+graphics.Color(239, 202, 36),
+graphics.Color(241, 208, 30),
+graphics.Color(244, 213, 24),
+graphics.Color(247, 218, 18),
+graphics.Color(250, 223, 12),
+graphics.Color(252, 229, 6),
+graphics.Color(255, 234, 0)]
 
+        now = datetime.now()
+        pos = ceil(64*now.second/60)
+        offscreen_canvas.Clear()
+        len = graphics.DrawText(offscreen_canvas, font, pos, 10, colors[ceil(31*now.second/60)], my_text)
+        return len
 # Main function
 if __name__ == "__main__":
     countdown = Countdown()
     if (not countdown.process()):
         countdown.print_help()
 
+try:
+    print("Press CTRL-C to stop.")
+    while True:
+        time.sleep(100)
+except KeyboardInterrupt:
+    sys.exit(0)
