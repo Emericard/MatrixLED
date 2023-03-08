351e6580c5578d97c0f931ab50df2b10adc3c57e
Coline <coline.droxler@epfl.ch>
2023-03-08 13:31:13 +0100

    +10

--- bindings/python/samples/countdown.py
+++ bindings/python/samples/countdown.py
@@ -86,11 +86,7 @@ graphics.Color(252, 229, 6),
 graphics.Color(255, 234, 0)]
         now = datetime.datetime.now()
         pos -= 0
-        if pos + length == 0:
-            pos = canvas.width
         lenght = graphics.DrawText(canvas, font, pos, 10, colors[ceil(31*now.second/60)], my_text)
-        for i in range(10):
-            lenght= graphics.DrawLine(canvas, 0, i, 20, i, graphics.Color(0,0,0))
         return pos, lenght
     
     def run_deadlines(self, canvas, pos, length, text = "Coucou!"):
